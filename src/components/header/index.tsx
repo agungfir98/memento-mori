0a1898f9ad6679ff4dd929d7a740b8026ede1dbf
agungfir98 <agungfir98@gmail.com>
2023-07-25 15:41:06 +0700

    swaping kalender hidup dengan memento mori

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,8 +5,8 @@ const roboto = Roboto({ subsets: ['latin'], weight: '700' })
 export default function Header() {
   return (
     <header className={`${roboto.className} text-center  pt-4`}>
-      <h1 className="text-5xl font-bold">KALENDER HIDUP</h1>
-      <h2 className="text-md text-black/65">Memento Mori</h2>
+      <h1 className="text-5xl font-bold uppercase">memento mori</h1>
+      <h2 className="text-md text-black/65">Kalender kehidupanmu</h2>
     </header>
   )
 }
