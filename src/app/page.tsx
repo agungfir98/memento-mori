8f405878952893d5da436d7e08d9d56bf0aeb65c
agungfir98 <agungfir98@gmail.com>
2023-07-25 13:10:45 +0700

    change color and add descriptive text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,44 @@
 
 import Header from 'C/header'
 import WeekTiles from 'C/weekTiles'
+import moment from 'moment'
 
 export default function Home() {
   return (
     <div className="mx-10 flex flex-col gap-8">
       <Header />
-      <main className="">
+      <main className="grid max-sm:grid-cols-1 sm:grid-cols-3">
         <WeekTiles />
+        <ul className="list-disc text-justify font-semibold leading-7 pb-20 sm:col-start-3 sm:pl-12">
+          <li className="text-green-600">
+            Pada rentang ini, kamu hanya seorang anak anak, menikmati hidup
+            dengan bermain disebagian besar waktumu.
+          </li>
+          <li className="text-cyan-600">
+            Pada rentang ini kamu mulai memasuki masa remaja. kamu mungkin mulai
+            merasakan jatuh cinta, dan patah hati untuk pertama kalinya, dan
+            senang melakukan hal yang benar benar baru untuk mencari jati diri.
+          </li>
+          <li className="text-orange-600">
+            pada rentang ini kamu mungkin mulai melanjutkan pendidikanmu ke
+            jenjang yang lebih tinggi ataupun memilih untuk bekerja. kamu mulai
+            merasakan dunia yang sesungguhnya, menjadi bagian dari masyarakat.
+          </li>
+          <li className="text-red-600">
+            pada rentang ini kamu mungkin sudah menjadi manusia seutuhnya
+            menurut standar masyarakat seperti menikah, memiliki anak, mencapai
+            segala keinginan yang kamu punya, atau hidup dengan nyaman dari
+            hasil bekerja. Ini adalah bagian terpanjang dalam hidupmu.
+          </li>
+          <li className="text-slate-600">
+            pada rentang ini kamu mungkin sudah pensiun, menuai segala yang
+            telah kamu tanam semasa hidup.
+          </li>
+        </ul>
       </main>
+      <footer className="text-center pb-5">
+        ©️{moment().year()} made with 💗 by Agung as a memento mori
+      </footer>
     </div>
   )
 }
--- src/components/weekTiles/index.tsx
+++ src/components/weekTiles/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import moment, { Moment } from 'moment'
-// import { Tooltip as ReactToolTip } from 'react-tooltip'
 import 'react-tooltip/dist/react-tooltip.css'
 
 export default function WeekTiles() {
@@ -36,53 +35,43 @@ export default function WeekTiles() {
     const weekList = document.querySelectorAll('#weekList')
     for (let i = 0; i <= weekPassed - 1; i += 1) {
       if (i >= weekInYear * 60) {
-        weekList[i]?.classList.add('bg-red-500')
+        weekList[i]?.classList.add('bg-slate-600')
       }
-      if (i >= weekInYear * 25 && i < weekInYear * 60) {
-        weekList[i]?.classList.add('bg-orange-300')
+      if (i >= weekInYear * 26 && i < weekInYear * 60) {
+        weekList[i]?.classList.add('bg-red-600')
       }
-      if (i >= weekInYear * 18 && i < weekInYear * 25) {
-        weekList[i]?.classList.add('bg-cyan-300')
+      if (i >= weekInYear * 17 && i < weekInYear * 26) {
+        weekList[i]?.classList.add('bg-orange-600')
       }
-      if (i >= weekInYear * 0 && i < weekInYear * 18) {
-        weekList[i]?.classList.add('bg-green-400')
+      if (i >= weekInYear * 12 && i < weekInYear * 17) {
+        weekList[i]?.classList.add('bg-cyan-600')
+      }
+      if (i >= weekInYear * 0 && i < weekInYear * 12) {
+        weekList[i]?.classList.add('bg-green-600')
       }
     }
 
     for (let j = 0; j <= totalWeeks; j += 1) {
       if (j >= weekInYear * 60) {
-        weekList[j]?.classList.add('outline-red-500')
+        weekList[j]?.classList.add('outline-slate-600')
       }
-      if (j >= weekInYear * 25 && j < weekInYear * 60) {
-        weekList[j]?.classList.add('outline-orange-300')
+      if (j >= weekInYear * 26 && j < weekInYear * 60) {
+        weekList[j]?.classList.add('outline-red-600')
       }
-      if (j >= weekInYear * 18 && j < weekInYear * 25) {
-        weekList[j]?.classList.add('outline-cyan-300')
+      if (j >= weekInYear * 17 && j < weekInYear * 26) {
+        weekList[j]?.classList.add('outline-orange-600')
       }
-      if (j >= weekInYear * 0 && j < weekInYear * 18) {
-        weekList[j]?.classList.add('outline-green-400')
+      if (j >= weekInYear * 12 && j < weekInYear * 17) {
+        weekList[j]?.classList.add('outline-cyan-600')
+      }
+      if (j >= weekInYear * 0 && j < weekInYear * 12) {
+        weekList[j]?.classList.add('outline-green-600')
       }
     }
   }, [totalWeeks, birth, weekPassed, weekInYear])
 
-  const handleMouseEnter = (index: number) => {
-    const weekList = document.querySelectorAll('#weekList')
-    const isActivated = false
-
-    if (isActivated) {
-      for (let i = 0; i <= index; i += 1) {
-        weekList[i]?.classList.add('')
-      }
-
-      for (let j = totalWeeks; j > index; j -= 1) {
-        weekList[j]?.classList.remove('')
-      }
-    }
-  }
-  // <ReactToolTip place="top" id="weekList" content="hehe" />
-
   return (
-    <div className="grid grid-cols-[auto,1fr] pb-20">
+    <div className="grid grid-cols-[auto,1fr] pb-20 col-span-2">
       <div className="col-start-2 gap-2 flex items-center font-semibold">
         Pekan dalam hidupmu<p className="text-3xl text-top">&#8640;</p>
       </div>
@@ -92,7 +81,7 @@ export default function WeekTiles() {
       >
         {[...Array(80)].map((_, i) => (
           <div key={i} className="mr-2 relative flex items-center justify-end">
-            <p className="text-end font-semibold -top-4 -left-2 text-sm">
+            <p className="text-end absolute font-semibold sm:-top-1 text-sm">
               {/* eslint-disable-next-line no-nested-ternary */}
               {i + 1 === 1 ? i + 1 : (i + 1) % 5 ? null : i + 1}
             </p>
@@ -102,13 +91,10 @@ export default function WeekTiles() {
       <div className="grid gap-2 grid-flow-row grid-cols-[repeat(26,minmax(0,1fr))] auto-rows-max auto-cols-max md:grid-cols-[repeat(52,minmax(0,1fr))]">
         {tiles.map((_, i) => (
           <div
-            onMouseEnter={() => handleMouseEnter(i)}
             id="weekList"
             key={i}
             className="rounded-full relative outline-2 outline pt-[100%] box-border"
-          >
-            {/** <p className="absolute top-0 left-0">{i + 1}</p> */}
-          </div>
+          />
         ))}
       </div>
     </div>
